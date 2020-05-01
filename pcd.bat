07f5c902b557907dadd6c080d61d6fdb5550fada
kita127 <kitada.kosuke@gmail.com>
2020-05-01 12:53:40 +0900

    add pcd.bat

---
+++ pcd.bat
@@ -0,0 +1,46 @@
+@echo off
+
+if "%1" equ "add" (
+  if "%2" equ "." (
+    cd >> "%USERPROFILE%\.peco-cd"
+    goto end
+  )
+
+  if "%2" neq "" (
+    echo %2 >> "%USERPROFILE%\.peco-cd"
+    goto end
+  )
+  goto usage
+)
+if "%1" equ "edit" (
+  goto edit
+)
+if "%1" neq "" (
+  goto usage
+)
+goto query
+
+:usage
+echo %0 [add PATH ^| edit]
+goto end
+
+:edit
+if "%EDITOR%" neq "" (
+  "%EDITOR%" "%USERPROFILE%\.peco-cd"
+  goto end
+)
+gvim "%USERPROFILE%\.peco-cd"
+goto end
+
+:query
+rem NOTE:
+rem
+rem If you have a problem caused by character-set, modify below part like:
+rem   'type ^"%USERPROFILE%\.peco-cd^" ^| iconv -f char -t utf-8 ^| peco'
+rem
+for /f %%i in ('type ^"%USERPROFILE%\.peco-cd^" ^| peco') do (
+  cd %%i
+  break
+)
+
+:end
